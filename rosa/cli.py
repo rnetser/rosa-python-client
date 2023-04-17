55fba778d6690910e10bcb9e354004087bd385a5
Meni Yakove <myakove@gmail.com>
2023-04-17 16:05:29 +0300

    Fix json parse

--- rosa/cli.py
+++ rosa/cli.py
@@ -84,7 +84,9 @@ def execute(command, allowed_commands=None):
     command.extend(_user_command)
     json_output = None
     for cmd in command[1:]:
-        json_output = allowed_commands[cmd] if not json_output else json_output[cmd]
+        json_output = allowed_commands[cmd] if not json_output else json_output.get(cmd)
+        if json_output:
+            break
 
     if json_output:
         command.append("-ojson")
