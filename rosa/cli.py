40ec972dbf7685388c3dc9511e2476cea5068678
Ruth Netser <rnetser@redhat.com>
2023-06-14 20:33:40 +0300

    Add region to ROSA commands (#18)
    * Add region to ROSA commands
    
    * Add region to ROSA commands
    
    * Add missing params
    
    * Update required params

--- rosa/cli.py
+++ rosa/cli.py
@@ -10,7 +10,6 @@ from simple_logger.logger import get_logger
 
 
 LOGGER = get_logger(__name__)
-ROSA_STR = "rosa"
 
 
 class CommandExecuteError(Exception):
@@ -22,14 +21,14 @@ class NotLoggedInError(Exception):
 
 
 @contextlib.contextmanager
-def rosa_login_logout(env, token):
-    execute_command(
-        command=shlex.split(
-            f"{ROSA_STR} login {f'--env={env}' if env else ''} --token={token}"
-        )
+def rosa_login_logout(env, token, aws_region, allowed_commands=None):
+    _allowed_commands = allowed_commands or parse_help()
+    build_execute_command(
+        command=f"login --region {aws_region} {f'--env={env}' if env else ''} --token={token}",
+        allowed_commands=_allowed_commands,
     )
     yield
-    execute_command(command=shlex.split(f"{ROSA_STR} logout"))
+    build_execute_command(command="logout", allowed_commands=_allowed_commands)
 
 
 @contextlib.contextmanager
@@ -40,9 +39,12 @@ def change_home_environment():
     os.environ["HOME"] = current_home
 
 
-def is_logged_in():
+def is_logged_in(aws_region=None, allowed_commands=None):
+    _allowed_commands = allowed_commands or parse_help()
     try:
-        res = execute_command(command=shlex.split(f"{ROSA_STR} whoami"))
+        res = build_execute_command(
+            command="whoami", aws_region=aws_region, allowed_commands=_allowed_commands
+        )
         return "User is not logged in to OCM" not in res["err"]
     except CommandExecuteError:
         return False
@@ -51,7 +53,7 @@ def is_logged_in():
 def execute_command(command):
     joined_command = " ".join(command)
     LOGGER.info(
-        f"Executing command: {re.sub(r'--token=.*', '--token=hashed-token', joined_command)}"
+        f"Executing command: {re.sub(r'(--token=.* |--token=.*)', '--token=hashed-token ', joined_command)}"
     )
     res = subprocess.run(command, capture_output=True, text=True)
     if res.returncode != 0:
@@ -68,34 +70,42 @@ def check_flag_in_flags(command_list, flag_str):
     return False
 
 
-def build_command(command, allowed_commands=None):
-    allowed_commands = allowed_commands or parse_help()
+def build_command(command, allowed_commands=None, aws_region=None):
+    _allowed_commands = allowed_commands or parse_help()
     _user_command = shlex.split(command)
-    command = [ROSA_STR]
+    command = ["rosa"]
     command.extend(_user_command)
     json_output = {}
     auto_answer_yes = {}
     auto_update = {}
+    aws_region_flag = {}
     for cmd in command[1:]:
         if cmd.startswith("--"):
             continue
 
-        json_output = allowed_commands.get(cmd, json_output.get(cmd, {}))
+        json_output = _allowed_commands.get(cmd, json_output.get(cmd, {}))
         add_json_output = json_output.get("json_output") is True
         if add_json_output:
             command.append("-ojson")
 
-        auto_answer_yes = allowed_commands.get(cmd, auto_answer_yes.get(cmd, {}))
+        auto_answer_yes = _allowed_commands.get(cmd, auto_answer_yes.get(cmd, {}))
         add_auto_answer_yes = auto_answer_yes.get("auto_answer_yes") is True
         if add_auto_answer_yes:
             command.append("--yes")
 
-        auto_update = allowed_commands.get(cmd, auto_update.get(cmd, {}))
+        auto_update = _allowed_commands.get(cmd, auto_update.get(cmd, {}))
         add_auto_update = auto_update.get("auto_mode") is True
         if add_auto_update:
             command.append("--mode=auto")
 
-        if any([add_json_output, add_auto_answer_yes, add_auto_update]):
+        aws_region_flag = _allowed_commands.get(cmd, aws_region_flag.get(cmd, {}))
+        add_aws_region_flag = json_output.get("region") is True
+        if add_aws_region_flag and aws_region:
+            command.append(f"--region={aws_region}")
+
+        if any(
+            [add_json_output, add_auto_answer_yes, add_auto_update, add_aws_region_flag]
+        ):
             break
 
     return command
@@ -141,6 +151,7 @@ def parse_help(rosa_cmd="rosa"):
     output_flag_str = "-o, --output"
     auto_answer_yes_str = "-y, --yes"
     auto_mode_str = "-m, --mode"
+    region_str = "--region"
 
     for command in _commands:
         commands_dict.setdefault(command, {})
@@ -171,6 +182,12 @@ def parse_help(rosa_cmd="rosa"):
                         command_list=[rosa_cmd, top_command, _command],
                         flag_str=auto_mode_str,
                     )
+                    commands_dict[top_command][command][_command][
+                        "region"
+                    ] = check_flag_in_flags(
+                        command_list=[rosa_cmd, top_command, _command],
+                        flag_str=region_str,
+                    )
             else:
                 commands_dict[top_command][command][
                     "json_output"
@@ -187,6 +204,10 @@ def parse_help(rosa_cmd="rosa"):
                 commands_dict[top_command][command]["auto_mode"] = check_flag_in_flags(
                     command_list=[rosa_cmd, top_command, command],
                     flag_str=auto_mode_str,
+                )
+                commands_dict[top_command][command]["region"] = check_flag_in_flags(
+                    command_list=[rosa_cmd, top_command, command],
+                    flag_str=region_str,
                 )
 
     return commands_dict
@@ -205,8 +226,21 @@ def parse_json_response(response):
     }
 
 
+def build_execute_command(command, allowed_commands=None, aws_region=None):
+    _allowed_commands = allowed_commands or parse_help()
+    command = build_command(
+        command=command, allowed_commands=_allowed_commands, aws_region=aws_region
+    )
+    return execute_command(command=command)
+
+
 def execute(
-    command, allowed_commands=None, ocm_env="production", token=None, ocm_client=None
+    command,
+    allowed_commands=None,
+    ocm_env="production",
+    token=None,
+    ocm_client=None,
+    aws_region=None,
 ):
     """
     Support commands and execute with ROSA cli
@@ -220,6 +254,7 @@ def execute(
         ocm_env (str): OCM env to log in into.
         token (str): Access or refresh token generated from https://console.redhat.com/openshift/token/rosa.
         ocm_client (OCMPythonClient): OCM client to use for log in.
+        aws_region (str): AWS region to use for ROSA commands.
 
     Example:
         allowed_commands = {'create':
@@ -234,20 +269,30 @@ def execute(
             res.stdout/stderr will be parsed as json if possible, else str
     """
     _allowed_commands = allowed_commands or parse_help()
+
     if token or ocm_client:
         if ocm_client:
             ocm_env = ocm_client.api_client.configuration.host
             token = ocm_client.api_client.token
 
-        with change_home_environment(), rosa_login_logout(env=ocm_env, token=token):
-            command = build_command(command=command, allowed_commands=_allowed_commands)
-            return execute_command(command=command)
+        with change_home_environment(), rosa_login_logout(
+            env=ocm_env,
+            token=token,
+            aws_region=aws_region,
+            allowed_commands=_allowed_commands,
+        ):
+            return build_execute_command(
+                command=command,
+                allowed_commands=_allowed_commands,
+                aws_region=aws_region,
+            )
 
     else:
-        if not is_logged_in():
+        if not is_logged_in(allowed_commands=_allowed_commands, aws_region=aws_region):
             raise NotLoggedInError(
                 "Not logged in to OCM, either pass 'token' or log in before running."
             )
 
-        command = build_command(command=command, allowed_commands=_allowed_commands)
-        return execute_command(command=command)
+        return build_execute_command(
+            command=command, allowed_commands=_allowed_commands, aws_region=aws_region
+        )
